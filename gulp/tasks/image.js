dc8842f8b5f855c573a5a9818d25b324f8124bbc
SeaiiaeS <hayk.adamyan.2002@gmail.com>
2022-06-12 23:57:29 +0400

    bags fixed with imagemin

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,14 +1,16 @@
-import imagemin from 'imagemin';
+import imagemin from 'gulp-imagemin';
 import imagemin_jpg from 'imagemin-jpegtran';
 import imagemin_png from 'imagemin-pngquant';
 
-export const image = await imagemin(['images/*.{jpg,png}'], {
-	destination: app.paths.build.imageFolder,
-	plugins: [
-		imagemin_jpg(),
-		imagemin_png({
-			quality: [0.6, 0.8]
-		})
-	]
-});
-//=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
+export const image = () => {
+    return app.gulp.src(app.path.imageFolder)
+        .pipe(imagemin([
+            imagemin_jpg({
+                quality:[0.7,0.9]
+            }),
+            imagemin_png({
+                quality: [0.7, 0.9]
+            })
+        ]))
+        .pipe(app.gulp.dest(app.path.build.imageFolder));
+    }
