230033394055afb445a38d01a999cda2579fc5fb
SeaiiaeS <hayk.adamyan.2002@gmail.com>
2022-06-12 23:36:41 +0400

    imagemin plugin added

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,8 +1,14 @@
-import imagemin from "gulp-imagemin";
-export const image=()=>{
-    return app.gulp.src(app.path.imageFolder)
-        .pipe(imagemin({
-            progressive: true,
-        }))
-        .pipe(app.gulp.dest(app.path.build.imageFolder));
-}
+import imagemin from 'imagemin';
+import imagemin_jpg from 'imagemin-jpegtran';
+import imagemin_png from 'imagemin-pngquant';
+
+export const image = await imagemin(['images/*.{jpg,png}'], {
+	destination: app.paths.build.imageFolder,
+	plugins: [
+		imagemin_jpg(),
+		imagemin_png({
+			quality: [0.6, 0.8]
+		})
+	]
+});
+//=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
